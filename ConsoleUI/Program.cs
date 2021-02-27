3a7367e40cfcd295be877261f70d5f885c00b184
serhatsevim <serhatsevim01@gmail.com>
2021-02-27 12:36:25 +0200

    Car Rental Project Backend V1

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+using Business.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CarManager carManager = new CarManager(new InMemoryCarDal());
+			//Verilen Id'si eşleşen arabanın bilgileri ekrara yazılır.
+			var car = carManager.GetById(3);
+			Console.WriteLine("Model Yılı : {0} Günlük Ücreti : {1} Detaylar : {2}",car.ModelYear, car.DailyPrice, car.Description);
+			//Yeni bir araba ekleme
+			carManager.Add(new Car{Id=6, BrandId=3, ColorId=2, ModelYear=2021, DailyPrice=200, Description="Navigasyon, Manuel vites, Klima"});
+			//Id ye göre araba günceleniyor
+			carManager.Update(new Car{Id=2, BrandId=2, ColorId=3, ModelYear=2012, DailyPrice=300, Description="Navigasyon, Otomatik Vites, Elektrikli Klima"});
+			//Id si gönderilen arabayı sil
+			carManager.Delete(new Car{Id=3});
+			//Tüm arabaları listele
+			foreach(var car in carManager.GetAll())
+			{
+				Console.WriteLine("Model Yılı : {0} Günlük Ücreti : {1} Detaylar : {2}",car.ModelYear, car.DailyPrice, car.Description);
+			}
+		}
+	}
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,55 @@
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+	public class InMemoryCarDal : ICarDal
+	{
+		List<Car> _cars;
+		public InMemoryCarDal()
+		{
+			_cars = new List<Car>{
+				new Car{Id=1, BrandId=1, ColorId=1, ModelYear=2010, DailyPrice=70, Description="Elektronik Klima, Navigasyon , Manuel Vites, Hava yastığı"},
+				new Car{Id=2, BrandId=2, ColorId=3, ModelYear=2012, DailyPrice=100, Description="Navigasyon, Otomatik Vites"},
+				new Car{Id=3, BrandId=1, ColorId=5, ModelYear=2015, DailyPrice=150, Description="Vip hizmete özel araç"},
+				new Car{Id=4, BrandId=3, ColorId=2, ModelYear=2017, DailyPrice=175, Description="Navigasyon, Hava yastığı, Otomatik vites"},
+				new Car{Id=5, BrandId=3, ColorId=4, ModelYear=2021, DailyPrice=250, Description="Elektronik Klima, Navigasyon, Hava yastığı, Manuel Vites"}
+			}; 
+		}
+		
+		public void Add(Car car)
+		{
+			_cars.Add(car);
+		}		
+			
+		public void Delete(Car car)
+		{
+			Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+			_cars.Remove(carToDelete);
+		}
+		
+		public List<Car> GetAll()
+		{
+			return _cars;
+		}
+		
+		public Car GetById(int id)
+		{
+			return _cars.SingleOrDefault(c => c.Id == id);
+		}		
+		
+		public void Update(Car car)
+		{
+			Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+			carToUpdate.BrandId = car.BrandId;
+			carToUpdate.ColorId = car.ColorId;
+			carToUpdate.ModelYear = car.ModelYear;
+			carToUpdate.DailyPrice = car.DailyPrice;
+			carToUpdate.Description = car.Description;
+		}
+		
+	}
+}
